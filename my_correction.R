918af378262290c666841becd9b71deb26ef92a3
Patricia Xiao <patricia.xiao@berkeley.edu>
2019-02-16 14:12:22 -0800

    updates

--- my_correction.R
+++ my_correction.R
@@ -1,3 +1,5 @@
+#R version 3.3.2 
+  
 rxnmix<- function(molecules, nrrxn){
     dimstart<-length(molecules)
     moleculestart<-sample(molecules,1)
@@ -23,6 +25,18 @@ rxnmix<- function(molecules, nrrxn){
     return(moleculesreaction)
 }
 
+repeats=10 # 0
+x=c(rep(0,110)) # 110 times 0 array
+
+
 for(j in 1:repeats) {
-    #
+    molecules12<-c(rep(12,200))
+    mix12<-rxnmix(molecules12, 2000) # 200 or 1000 or 2000 or 20000
+    densdat12<-hist(mix12,breaks=c(0:110),plot=FALSE)$density
+    # densdat12<-hist(mix12,breaks=c(0:110),plot=TRUE)$density
+    x=cbind(x,densdat12)
 }
+
+y=rowSums(x)/repeats
+
+# print(y)
