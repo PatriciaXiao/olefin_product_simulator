f7936a89deaa772c38b41cf211a64c6247d518a5
Patricia Xiao <patricia.xiao@berkeley.edu>
2019-02-16 17:43:02 -0800

    new updates

--- olefin.py
+++ olefin.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 def rxnmix(molecules, nrrxn):
     dimstart = len(molecules)
@@ -9,8 +10,8 @@ def rxnmix(molecules, nrrxn):
     moleculecleaved = [doublebond, moleculestart - doublebond]
     rest = np.random.choice(moleculecleaved)
     # Start of the reaction
-    moleculesreaction = molecules[:] # copy of the list
-    moleculesreaction.remove(moleculestart)
+    index = np.argwhere(molecules == moleculestart)[0]
+    moleculesreaction = np.delete(molecules, index)
     # Replace here means select nrrxn samples from moleculesreaction, could select the same element twice
     range_molecule_index = range(len(moleculesreaction))
     indexmoleculeselected = np.random.choice(range_molecule_index, nrrxn, replace=True)
@@ -28,8 +29,12 @@ def init_molecules12():
     return np.array([12] * 200)
 
 current_init = init_molecules12
-repeats = 1 #100
 n_steps = 200
+fname = "C12_{0}.csv".format(n_steps)
+
+repeats = 100
+max_slot = 110
+min_slot = 0
 n_slots = 110
 
 x_sum = np.zeros(n_slots)
@@ -37,8 +42,12 @@ x_sum = np.zeros(n_slots)
 for j in range(repeats):
     molecules = current_init()
     mix = rxnmix(molecules, n_steps) # 200 or 1000 or 2000 or 20000
-    densedat, bin_edges = np.histogram(mix, bins=110, density=True)
+    densedat, bin_edges = np.histogram(mix, bins=n_slots, density=True, range=(min_slot, max_slot))
     x_sum += densedat
 
 y = x_sum / float(repeats)
-print y
+
+d = {'y': y}
+df = pd.DataFrame(data=d)
+df.to_csv(fname, sep='\t')
+
